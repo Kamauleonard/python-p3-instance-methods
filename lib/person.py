29ec2d9f0ad70af5c4c22443e7da35976b8e74d5
Kamauleonard <leonardmwangi2012@gmail.com>
2023-12-12 11:27:56 +0300

    Completed Assignment

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
 
-    #Instance method definition
     pass
+    def talk(self):
+      print("Hello World!")
+    def walk(self):
+      print("The person is walking.")
+my_person = Person()
+my_person.talk()
+my_person.walk()
